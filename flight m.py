78047ce0e5238102c15502fe248cb3c31fca9511
dila5454 <85063574+dila5454@users.noreply.github.com>
2021-05-31 18:14:38 +0500

    file

--- flight m.py
+++ flight m.py
@@ -213,6 +213,232 @@ def user_management():  # this function is responsible for adding users
                     admin1.title("Success!")
                     tk.Label(master=admin1, text="Standard User Successfully Deleted!").grid(row=1, column=1)
                     delete_users_scheduled.destroy()
+        delete_users_scheduled = tk.Tk()
+        delete_users_scheduled.title("Delete A User")
+        tk.Label(master=delete_users_scheduled, text="Enter the Username").grid(row=1, column=0)
+        username = tk.Entry(master=delete_users_scheduled)
+        username.grid(row=1, column=1)
+        admin2 = tk.Button(master=delete_users_scheduled, text="Confirm Deletion", width=25,
+                           command=deleting_back_user).grid(row=1, column=2)
+
+    manage_user_scheduled = tk.Tk()
+    manage_user_scheduled.title("Manage Users")
+    tk.Button(master=manage_user_scheduled, text="Add Users", width=25, command=adding_user).grid(row=1, column=0)
+    tk.Button(master=manage_user_scheduled, text="Delete A User", width=25, command=deleting_users).grid(row=2,
+                                                                                                         column=0)
+    tk.Button(master=manage_user_scheduled, text="View Current Users", width=25, command=display_users).grid(row=3,
+                                                                                                             column=0)
+
+
+def update():
+    scheduled_update = tk.Tk()
+    scheduled_update.title("Update/Add A Flight")
+    tk.Label(master=scheduled_update, text="Enter The Flight Number").grid(row=1, column=0)
+    number_of_flight = tk.Entry(master=scheduled_update)
+    number_of_flight.grid(row=1, column=1)
+
+    def update_btn1():
+        if number_of_flight.get() not in scheduled:
+            scheduled[number_of_flight.get()] = ["", "", ""]
+        tk.Label(master=scheduled_update, text="Enter Departure Time").grid(row=3, column=0)
+        departure_time = tk.Entry(master=scheduled_update)
+        departure_time.grid(row=3, column=1)
+        tk.Label(master=scheduled_update, text="Enter Status").grid(row=4, column=0)
+        stat = tk.Entry(master=scheduled_update)
+        stat.grid(row=4, column=1)
+        tk.Label(master=scheduled_update, text="Enter Destination").grid(row=5, column=0)
+        destination_place = tk.Entry(master=scheduled_update)
+        destination_place.grid(row=5, column=1)
+
+        def update_btn2():
+            if departure_time.get() != "":
+                scheduled[number_of_flight.get()][0] = departure_time.get()
+            if stat.get() != "":
+                scheduled[number_of_flight.get()][2] = stat.get()
+            if destination_place.get() != "":
+                scheduled[number_of_flight.get()][1] = destination_place.get()
+            update_root = tk.Tk()
+            update_root.title("Successfully Updated!")
+            tk.Label(master=update_root, text="Successfully Updated!!").grid(row=0, column=0)
+            scheduled_update.destroy()
+
+        tk.Button(master=scheduled_update, text="Confirm", command=update_btn2).grid(row=6, column=1)
+
+    tk.Button(master=scheduled_update, text="Confirm", command=update_btn1).grid(row=2, column=1)
+    scheduled_update.mainloop()
+
+
+def cancel():  # this function cancels flights
+    def back_cancelled():
+        flight = flight_number.get()
+        cancelled_scheduled.destroy()
+        if flight in cancelled:
+            root = tk.Tk()
+            root.title("Already Cancelled!")
+            tk.Label(master=root, text="Flight Already Cancelled. Please try again!").grid(row=1, column=1)
+            cancel()
+        elif not (flight in scheduled):
+            root = tk.Tk()
+            root.title("Flight Not Found!")
+            tk.Label(master=root, text="Flight Not Found. Please try again!").grid(row=1, column=1)
+            cancel()
+        else:
+            flight1 = scheduled.pop(flight)
+            del flight1[2]
+            flight1.append("Cancelled")
+            cancelled[flight] = flight1
+            root = tk.Tk()
+            root.title("Success!")
+            tk.Label(master=root, text="Flight Successfully Cancelled!").grid(row=1, column=1)
+
+    cancelled_scheduled = tk.Tk()
+    cancelled_scheduled.title("Cancel a Flight")
+    tk.Label(master=cancelled_scheduled, text="Enter the flight number").grid(row=1, column=0)
+    flight_number = tk.Entry(master=cancelled_scheduled)
+    flight_number.grid(row=1, column=1)
+    admin7 = tk.Button(master=cancelled_scheduled, width=25, text="Confirm", command=back_cancelled).grid(row=2,
+                                                                                                          column=1)
+
+
+def admin_main_features():  # admin control panel
+    def switch_users_admin():
+        admin_main_scheduled.destroy()
+        login()
+
+    admin_main_scheduled = tk.Tk()
+    admin_main_scheduled.title("Admin Control Panel")
+    tk.Button(master=admin_main_scheduled, text="View The Details Of Flights", command=viewing_flights).grid(row=1,
+                                                                                                             column=1)
+    tk.Button(master=admin_main_scheduled, text="Switch User", command=switch_users_admin).grid(row=2, column=1)
+    tk.Button(master=admin_main_scheduled, text="Cancel A Flight", command=cancel).grid(row=4, column=1)
+    tk.Button(master=admin_main_scheduled, text="Manage Users", command=user_management).grid(row=5, column=1)
+    tk.Button(master=admin_main_scheduled, text="Exit The Program", command=exit).grid(row=6, column=1)
+    tk.Button(master=admin_main_scheduled, text="Update/Add A Flight", command=update).grid(row=3, column=1)
+
+
+def manager_main():  # supervisor control panel
+    def switch_users_manager():
+        manager_main_scheduled.destroy()
+        login()
+
+    manager_main_scheduled = tk.Tk()
+    image = tk.PhotoImage(file="icon.png")
+    ask = tk.Label(master=manager_main_scheduled, image=image).grid(row=0, column=0)
+    manager_main_scheduled.title("Supervisor Control Panel")
+    tk.Label(master=manager_main_scheduled, text="").grid(row=6, column=0)
+    tk.Button(master=manager_main_scheduled, text="View The Details Of Flights", command=viewing_flights).grid(row=1,
+                                                                                                               column=1)
+    tk.Button(master=manager_main_scheduled, text="Switch User", command=switch_users_manager).grid(row=2, column=1)
+    tk.Button(master=manager_main_scheduled, text="Cancel A Flight", command=cancel).grid(row=4, column=1)
+    tk.Button(master=manager_main_scheduled, text="Exit The Program", command=exit).grid(row=5, column=1)
+    tk.Button(master=manager_main_scheduled, text="Update/Add A Flight", command=update).grid(row=3, column=1)
+    manager_main_scheduled.mainloop()
+
+
+def viewing_flights():  # viewing flights
+    can = 0
+    ret = 1
+    display = tk.Tk()
+    display.title("View Details Of Flights")
+    tk.Label(master=display, text="Flight Number--------ETA--------Destination-------Status").grid(row=1, column=0)
+    for i in scheduled:
+        can += 1
+        ret += 1
+        tk.Label(master=display,
+                 text=(i, "-------", scheduled[i][0], "--------", scheduled[i][1], "--------", scheduled[i][2])).grid(
+            row=ret, column=0)
+    for i in cancelled:
+        can += 1
+        ret += 1
+        tk.Label(master=display,
+                 text=(i, "-------", cancelled[i][0], "--------", cancelled[i][1], "--------", cancelled[i][2])).grid(
+            row=ret, column=0)
+
+
+def main_standard():  # standard user control panel
+    def switch_user_standard():
+        main_standard_scheduled.destroy()
+        login()
+
+    main_standard_scheduled = tk.Tk()
+    main_standard_scheduled.title("Standard User Control Panel")
+    tk.Button(master=main_standard_scheduled, text="View The Details Of Flights", command=viewing_flights).grid(row=2,
+                                                                                                                column=0)
+    tk.Button(master=main_standard_scheduled, text="Switch User", command=switch_user_standard).grid(row=3, column=0)
+    tk.Button(master=main_standard_scheduled, text="Exit The Program", command=exit).grid(row=4, column=0)
+    tk.Label(master=main_standard_scheduled, text="").grid(row=5, column=0)
+    img = tk.PhotoImage(file="icon.png")
+    no = tk.Label(master=main_standard_scheduled, image=img).grid(row=1, column=0)
+    main_standard_scheduled.mainloop()
+
+
+def login():  # login function
+    def user_verification():
+        def password_verification():
+            pas = password.get()
+            if check == 1:
+                if admin[a] == pas:
+                    login_sched.destroy()
+                    admin_main_features()
+                    reminder()
+                else:
+                    admin3 = tk.Tk()
+                    admin3.title("Wrong Password!")
+                    tk.Label(master=admin3, text="Wrong Password! Please Try Again!").grid(row=1, column=1)
+
+            elif check == 2:
+                if manager[a] == pas:
+                    login_sched.destroy()
+                    manager_main()
+                    reminder()
+                else:
+                    admin3 = tk.Tk()
+                    admin3.title("Wrong Password!")
+                    tk.Label(master=admin3, text="Wrong Password! Please Try Again!").grid(row=1, column=1)
+            elif check == 3:
+                if standard[a] == pas:
+                    login_sched.destroy()
+                    main_standard()
+
+                else:
+                    admin3 = tk.Tk()
+                    admin3.title("Wrong Password!")
+                    tk.Label(master=admin3, text="Wrong Password! Please Try Again!").grid(row=1, column=1)
+
+        a = username.get()
+
+        if not (a in admin or a in manager or a in standard):
+            admin1 = tk.Tk()
+            admin1.title("Wrong Username!")
+            tk.Label(master=admin1, text="Username Not Found. Please Try Again!").grid(row=1, column=1)
+        else:
+            if a in manager:
+                check = 2
+            elif a in admin:
+                check = 1
+            elif a in standard:
+                check = 3
+
+            tk.Label(master=login_sched, text="Enter Your Password").grid(row=2, column=0)
+            password = tk.Entry(master=login_sched, show='*')
+            password.grid(row=2, column=1)
+            admin3 = tk.Button(master=login_sched, text="Confirm Password", width=25,
+                               command=password_verification).grid(row=2, column=2)
+
+    login_sched = tk.Tk()
+    login_sched.title("Login")
+    tk.Label(master=login_sched, text="").grid(row=5, column=1)
+    image = tk.PhotoImage(file="icon.png")
+    no = tk.Label(master=login_sched, image=image).grid(row=0, column=1)
+    tk.Label(master=login_sched, text="Enter Your Username").grid(row=1, column=0)
+    username = tk.Entry(master=login_sched)
+    username.grid(row=1, column=1)
+    asking = tk.Button(master=login_sched, text="Confirm Username", width=25, command=user_verification).grid(row=1,
+                                                                                                              column=2)
+    login_sched.mainloop()
+
+
+login()
 
        
 
